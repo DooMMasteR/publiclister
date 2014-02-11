3cb21f85a1be522ffe3d955b18194522ca2c5f2c
Steffen Arntz <winrootkit@gmail.com>
2014-02-11 12:07:56 +0100

    [feature] distiguish by years and print basic information of publication

--- publiclister.py
+++ publiclister.py
@@ -9,3 +9,16 @@ print("pub.xlsx loaded.")
 sheet = document.sheet_by_index(0)
 rows = sheet.nrows
 print("Table with " + str(rows) + " rows loaded.")
+
+current_row = 1
+
+while(current_row < rows):
+	# print sheet.row(current_row)
+	if (sheet.cell_type(current_row, 7) != 0) or (sheet.cell_type(current_row, 6) != 0):
+		print ("Year: " + str(sheet.cell_value(current_row, 5))),
+		print ("Type: " + sheet.cell_value(current_row, 6).encode('utf8')),
+		print ("Author: " + sheet.cell_value(current_row, 7).encode('utf8')),
+		print ("Title: " + sheet.cell_value(current_row, 12).encode('utf8'))
+	else:
+		print "NEW YEAR"
+	current_row += 1
